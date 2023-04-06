98297448115561a60cb434f039640f822354b585
leoniddavidchuk <49954415+LeonidDavidchuk@users.noreply.github.com>
2023-04-06 10:16:40 +0300

    add register component

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../register/Register.css";
 import arrow2 from "./images/arrow2.svg";
+import { Form } from "react-bootstrap";
+import rectangle from "./images/rectangle.svg";
 
 function Register() {
   return (
     <div className="register-container">
       <div className="text-container">
         <strong className="text1">
+          <img className="rectangle" src={rectangle} alt="rectangle" />
           ГОЛОСУЙ ЗА НАЙКРУТІШИЙ ДИЗАЙН УЧНІВСЬКОГО
         </strong>
         <span className="text2">
@@ -19,6 +22,28 @@ function Register() {
         </span>
 
         <img src={arrow2} alt="arrow2" />
+        <div className="Button-and-form">
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="ПІБ"
+              className="Form.Control"
+              style={{
+                position: "relative",
+                fontSize: "22px",
+                borderRadius: "50px",
+                borderColor: "black",
+                width: "560px",
+                height: "75px",
+                textIndent: "50px",
+              }}
+            />
+          </Form.Group>
+
+          <div className="registration">
+            <button>Зареєструватися</button>
+          </div>
+        </div>
       </div>
     </div>
   );
